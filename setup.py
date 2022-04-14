ff956ddd3200ee8e80cf7b70e2f667c99a6a87ec
AstroAllande02 <xuancern@gmail.com>
2022-04-14 12:27:48 +0200

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='allsky',
+    version='0.0.0',
+    description='Software to control the allsky camera of the Cereceda observatory',
+    packages=setuptools.find_packages(),
+    package_data={"allsky" :[],}
+)
